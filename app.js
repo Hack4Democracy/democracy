ffef22bd2377e52681e1be8f9ede1bcc125730d5
chieni <mitchieni@gmail.com>
2017-04-22 13:20:15 -0400

    handlebars

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var hbs = require('hbs')
+hbs.registerPartials(__dirname + '/templates');
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
